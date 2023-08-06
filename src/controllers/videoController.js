936c840748a0b1cd2e9c64368dc5b8e072c0999e
yjh9401 <ydh8@naver.com>
2023-08-06 12:44:11 +0900

    mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,8 +2,36 @@ const mockUser={
     username:"bnb",
     loggedIn:true
 }
-
-export const trendings = (req, res) => res.render("home", { pageTitle : "Home", mockUser });
+const videos=[];
+/*
+const videos=[
+    {
+        title: "first video",
+        rating: 5,
+        comment: 2,
+        createAt: "2 minute ago",
+        views: 52,
+        id: 1
+    },
+    {
+        title: "second video",
+        rating: 3,
+        comment: 15,
+        createAt: "1 month ago",
+        views: 61,
+        id: 2
+    },
+    {
+        title: "third video",
+        rating: 3,
+        comment: 108,
+        createAt: "1.5 month ago",
+        views: 1088,
+        id: 3
+    },
+]; //mock up data 만들기, array의 특징 length를 가진다
+*/
+export const trendings = (req, res) => res.render("home", { pageTitle : "Home", mockUser, videos });
 export const search = (req, res) => res.send("search video!! {videoController}");
 
 export const upload = (req, res) => res.send("videos upload!!! {videoController}");
